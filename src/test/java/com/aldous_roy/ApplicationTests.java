67dc41e9e36a4f4e6a676cec0bcd5beefde40362
Aldous Roy <aldousroy007@gmail.com>
2025-06-16 18:33:30 +0530

    inital

---
+++ src/test/java/com/aldous_roy/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldous_roy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
